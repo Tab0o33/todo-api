b866649cc2c7893779142edb61a75c898bd84d16
Tab0o33 <w.moinet@orange.fr>
2024-02-12 15:12:26 +0100

    refactor: creation d'un Dto pour typer l'objet en entrée de la route Post

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { Todo } from './interfaces/todo.interface';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
 export class TodosController {
@@ -19,7 +20,7 @@ export class TodosController {
     }
 
     @Post()
-    createTodo(@Body() newTodo) {
+    createTodo(@Body() newTodo: CreateTodoDto) {
         this.todosService.create(newTodo);
     }
 }
